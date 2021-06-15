6228e6855515515a922f0e99ff1cdb93d03d7df2
Abigayle Barnes <85874875+Wolfingard@users.noreply.github.com>
2021-06-15 17:00:06 +0100

    Activities done whilst learning

---
+++ activity3.2.py
@@ -0,0 +1,27 @@
+
+age = 16
+if age == 17:
+    print("Yes,I can serve you!")
+else :
+    print("You aren't old enough yet, sorry! Maybe in a few years.")
+
+animal = "Large Dog"
+if animal == "Large Dog":
+    print("Oh my, what a cute pupper!")
+else:
+    print("Oh my, Gremlins are creepy.")
+age = 16
+country = "UK" 
+
+if age > 17 and country == "UK":
+    print("Yes, I can serve you!")
+elif age > 17 and country != "UK":
+    print("Where you from friend?")
+else:
+    print("You aren't old enough yet, sorry!")
+
+password = 5
+if password == 8:
+    print ("Password")
+else:
+    print("Password is too short. Please try again.")
